c0dd4a535d451c57904a033136809b95fdb65722
ziontee113 <ziontee113@gmail.com>
2024-06-09 17:41:21 +0700

    Set texture filter

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const raylib = @import("raylib");
 pub fn main() !void {
     raylib.SetConfigFlags(raylib.ConfigFlags{ .FLAG_WINDOW_RESIZABLE = false, .FLAG_WINDOW_TRANSPARENT = true });
     raylib.InitWindow(800, 800, "hello world!");
+    raylib.SetTextureFilter(raylib.GetFontDefault().texture, @intFromEnum(raylib.TextureFilter.TEXTURE_FILTER_BILINEAR));
     raylib.SetTargetFPS(60);
 
     defer raylib.CloseWindow();
@@ -15,6 +16,11 @@ pub fn main() !void {
         raylib.ClearBackground(raylib.BLANK);
         raylib.DrawFPS(10, 10);
 
-        raylib.DrawText("FEARLESS", 100, 100, 20, raylib.YELLOW);
+        raylib.DrawText("FEARLESS", 0, 100, 10, raylib.YELLOW);
+        raylib.DrawText("FEARLESS", 200, 100, 12, raylib.YELLOW);
+        raylib.DrawText("FEARLESS", 0, 200, 14, raylib.YELLOW);
+        raylib.DrawText("FEARLESS", 200, 200, 16, raylib.YELLOW);
+        raylib.DrawText("FEARLESS", 0, 300, 18, raylib.YELLOW);
+        raylib.DrawText("FEARLESS", 200, 300, 20, raylib.YELLOW);
     }
 }
